53ae3a41f8d7619c73c0e86176b5500769861149
DzMS <dzigmihail@gmail.com>
2026-01-15 20:30:52 +0200

    Add 1.txt and hello.py files

---
+++ hello.py
@@ -0,0 +1,13 @@
+print('Hello world')
+first_number = 10
+second_number = first_number
+
+first_number = 20
+
+# first_number.change()
+
+firstNumber = 20
+FirstNumber = 20
+
+# + - * / // %
+first_number = first_number + second_number
